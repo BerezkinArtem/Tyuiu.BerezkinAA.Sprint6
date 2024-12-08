bd625db1eb23c256906eedc1e529eba86b8ef69c
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-08 22:52:52 +0500

    Add project files.

---
+++ Tyuiu.BerezkinAA.Sprint6.Task0.V1.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.BerezkinAA.Sprint6.Task0.V1.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.BerezkinAA.Sprint6.Task0.V1.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BerezkinAA.Sprint6.Task0.V1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
